73d597f905570b8d7bb5f76bd49dcae8b36c9698
Dmitry Palaznik <dpalaznik@gmail.com>
2016-09-12 18:47:55 +0300

    Some experiments with Hex and Exception handling in ExecutorServices

--- src/main/java/com/my/research/and/dev/HexAndBooleanAlg.java
+++ src/main/java/com/my/research/and/dev/HexAndBooleanAlg.java
@@ -1,7 +1,15 @@
 package com.my.research.and.dev;
 
-/**
- * Created by dmitry.palaznik on 05/09/2016.
- */
 public class HexAndBooleanAlg {
+
+    public static void main(String[] args) {
+        final long zeroEight = 0x00000008L;
+        final long eight = 0xFFFFFFF8L;
+        System.out.println(zeroEight);
+        System.out.println(eight);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE & zeroEight);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE & eight);
+    }
 }
--- src/main/java/com/my/research/and/dev/threads/ExceptionHandlingInExecutionService.java
+++ src/main/java/com/my/research/and/dev/threads/ExceptionHandlingInExecutionService.java
@@ -1,4 +1,28 @@
 package com.my.research.and.dev.threads;
 
+import java.util.concurrent.*;
+
 public class ExceptionHandlingInExecutionService {
+
+    public static void main(String[] args) throws Exception {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 1, TimeUnit.MINUTES, new LinkedBlockingQueue());
+        executor.submit(() -> System.out.println("Ok 1"));
+        Future<Integer> resultWithException = executor.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                System.out.println("Runtime Exception");
+                throw new RuntimeException("Got an Runtime Exception");
+
+            }
+        });
+        try {
+            System.out.println(resultWithException.get());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        executor.submit(() -> System.out.println("Ok 2"));
+        Future<Long> result = executor.submit(() -> System.currentTimeMillis() + 100);
+        System.out.println(result.get());
+        executor.shutdown();
+    }
 }
